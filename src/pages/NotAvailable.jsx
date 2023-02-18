41b8ada1656943c1c0e9de5fbecad1de2530d5e9
vivek sharma <vvk17sharma@gmail.com>
2023-02-19 00:39:35 +0530

    tvshow section done

--- src/pages/NotAvailable.jsx
+++ src/pages/NotAvailable.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import styled from'styled-components'
 
 export default function NotAvailable() {
   return (
-    <div>Not Available</div>
+    <div className="not-available">Sorry no video available for this :/ </div>
   )
 }
+
+const Container = styled.div`
+    .data{
+        margin-top:8rem;
+        .not-available{
+            text-align: center;
+            color:white;
+            margin-top:4rem:
+        }
+    }
+`;
